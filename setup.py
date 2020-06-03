47faa7b2b9afb94184667829c09190b1de5a9f3c
Seehait Chockthanyawat <s.chockthanyawat@gmail.com>
2020-06-03 11:55:29 +0700

    first version with a basic functionality

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os.path
+
+from setuptools import find_packages, setup
+
+setup(
+    name='series-renamer',
+    description='Series Renamer: rename files of series with respect to episode number.',
+    long_description=open(os.path.join(
+        os.path.dirname(__file__), 'README.md')).read(),
+    long_description_content_type='text/markdown',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=["natsort==7.0.1"],
+    url='https://github.com/seehait/series-renamer',
+    entry_points={
+        'console_scripts': ['series-renamer=src.main:main'],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
+    ],
+    license='MIT'
+)
