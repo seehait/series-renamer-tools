f67ea566fc826df9282983a24007c46fe986af43
Tal-D <tdan.m92@gmail.com>
2020-10-19 19:53:31 +0300

    Created name format changer basic unit tests class

--- test/test_series_renamer.py
+++ test/test_series_renamer.py
@@ -3,7 +3,6 @@ from os import path
 from src.main import change_files_name_format
 
 
-
 class MockedUserInput:
     def __init__(self, directory, prefix, is_dry_run):
         self.directory = directory
@@ -12,11 +11,7 @@ class MockedUserInput:
         self.files_extension = ".txt"
 
 
-class TestSeriesRenamerNotInDryMode(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.user_input = MockedUserInput("/test/", "family_matters", False)
-
+class SeriesRenamerTestsBaseClass(TestCase):
     def setUp(self):
         self.setUpPyfakefs()
 
@@ -26,18 +21,24 @@ class TestSeriesRenamerNotInDryMode(TestCase):
     def build_expected_file_path(self, episode_number):
         return self.user_input.directory + self.user_input.prefix + episode_number + self.user_input.files_extension
 
+    def create_input_files(self, number_of_files):
+        for file_index in range(number_of_files):
+            file_path = self.build_checked_file_path(file_index)
+            self.fs.create_file(file_path)
+            self.assertTrue(path.exists(file_path))
+
+
+class TestSeriesRenamerNotInDryMode(SeriesRenamerTestsBaseClass):
+    @classmethod
+    def setUpClass(cls):
+        cls.user_input = MockedUserInput("/test/", "family_matters", False)
+
     def validate_format_change(self, number_of_files):
         number_of_expected_file_digits = len(str(number_of_files))
         for file_index in range(number_of_files):
             episode_number = f"{file_index + 1}".zfill(number_of_expected_file_digits)
             self.assertFalse(path.exists(self.build_checked_file_path(file_index)))
             self.assertTrue(path.exists(self.build_expected_file_path(episode_number)))
-
-    def create_input_files(self, number_of_files):
-        for file_index in range(number_of_files):
-            file_path = self.build_checked_file_path(file_index)
-            self.fs.create_file(file_path)
-            self.assertTrue(path.exists(file_path))
 
     def test_single_digit_episodes(self):
         number_of_checked_episodes = 6
