b5aea36f2870348d27bcfa0c1ee96653253958fa
Tal-D <tdan.m92@gmail.com>
2020-10-19 20:42:03 +0300

    Added invalid directory unit tests for dry run and non dry run tests classes

--- test/test_series_renamer.py
+++ test/test_series_renamer.py
@@ -14,6 +14,7 @@ class MockedUserInput:
 class SeriesRenamerTestsBaseClass(TestCase):
     def setUp(self):
         self.setUpPyfakefs()
+        self.is_input_invalid = False
 
     def build_checked_file_path(self, file_index):
         return f"{self.user_input.directory}file{file_index}{self.user_input.files_extension}"
@@ -29,14 +30,14 @@ class SeriesRenamerTestsBaseClass(TestCase):
 
     def validate_old_file_name_deletion(self, file_index):
         original_file_path = self.build_checked_file_path(file_index)
-        if self.user_input.dry_run:
+        if self.user_input.dry_run or self.is_input_invalid:
             self.assertTrue(path.exists(original_file_path))
         else:
             self.assertFalse(path.exists(original_file_path))
 
     def validate_new_file_name_creation(self, episode_number):
         updated_file_path = self.build_expected_file_path(episode_number)
-        if self.user_input.dry_run:
+        if self.user_input.dry_run or self.is_input_invalid:
             self.assertFalse(path.exists(updated_file_path))
         else:
             self.assertTrue(path.exists(updated_file_path))
@@ -48,13 +49,24 @@ class SeriesRenamerTestsBaseClass(TestCase):
             self.validate_old_file_name_deletion(file_index)
             self.validate_new_file_name_creation(episode_number)
 
+    def add_invalid_directory(self):
+        self.is_input_invalid = True
+        self.user_input.directory = "5"
+
 
 class TestSeriesRenamerNotInDryMode(SeriesRenamerTestsBaseClass):
-    @classmethod
-    def setUpClass(cls):
-        cls.user_input = MockedUserInput("/test/", "family_matters", False)
+    def setUp(self):
+        SeriesRenamerTestsBaseClass.setUp(self)
+        self.user_input = MockedUserInput("/test/", "family_matters", False)
 
     def test_single_digit_episodes(self):
+        number_of_checked_episodes = 6
+        self.create_input_files(number_of_checked_episodes)
+        change_files_name_format(self.user_input)
+        self.validate_format_change(number_of_checked_episodes)
+
+    def test_single_digit_episodes_with_invalid_directory(self):
+        self.add_invalid_directory()
         number_of_checked_episodes = 6
         self.create_input_files(number_of_checked_episodes)
         change_files_name_format(self.user_input)
@@ -66,11 +78,18 @@ class TestSeriesRenamerNotInDryMode(SeriesRenamerTestsBaseClass):
         change_files_name_format(self.user_input)
         self.validate_format_change(number_of_checked_episodes)
 
+    def test_multiple_digits_episodes_with_invalid_directory(self):
+        self.add_invalid_directory()
+        number_of_checked_episodes = 77
+        self.create_input_files(number_of_checked_episodes)
+        change_files_name_format(self.user_input)
+        self.validate_format_change(number_of_checked_episodes)
+
 
 class TestSeriesRenamerInDryMode(SeriesRenamerTestsBaseClass):
-    @classmethod
-    def setUpClass(cls):
-        cls.user_input = MockedUserInput("/dry_test/", "the_office", True)
+    def setUp(self):
+        SeriesRenamerTestsBaseClass.setUp(self)
+        self.user_input = MockedUserInput("/dry_test/", "the_office", True)
 
     def test_single_digit_episodes(self):
         number_of_checked_episodes = 6
@@ -78,7 +97,21 @@ class TestSeriesRenamerInDryMode(SeriesRenamerTestsBaseClass):
         change_files_name_format(self.user_input)
         self.validate_format_change(number_of_checked_episodes)
 
+    def test_single_digit_episodes_with_invalid_directory(self):
+        self.add_invalid_directory()
+        number_of_checked_episodes = 6
+        self.create_input_files(number_of_checked_episodes)
+        change_files_name_format(self.user_input)
+        self.validate_format_change(number_of_checked_episodes)
+
     def test_multiple_digits_episodes(self):
+        number_of_checked_episodes = 77
+        self.create_input_files(number_of_checked_episodes)
+        change_files_name_format(self.user_input)
+        self.validate_format_change(number_of_checked_episodes)
+
+    def test_multiple_digits_episodes_with_invalid_directory(self):
+        self.add_invalid_directory()
         number_of_checked_episodes = 77
         self.create_input_files(number_of_checked_episodes)
         change_files_name_format(self.user_input)
