33af2d470a89eb9d1c9243b16f4fe68d94e95fc3
Tal-D <tdan.m92@gmail.com>
2020-10-20 21:01:49 +0300

    Small fix (setup tests level) in utils unit tests file

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,20 +1,23 @@
-import unittest
+from pyfakefs.fake_filesystem_unittest import TestCase
 from src.utils import parse_args, DIRECTORY_PARAMETER_VALID_KEYS, PREFIX_PARAMETER_VALID_KEYS,\
     DRY_RUN_PARAMETER_VALID_KEYS
 from os import path
 
 
-class TestUtils(unittest.TestCase):
+class TestUtils(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.prefix_name = "wheel_of_fortune"
-        cls.non_default_input_directory_suffix = "/parse_test/"
-        cls.non_default_full_input_directory = path.realpath(
-            cls.non_default_input_directory_suffix)
-        cls.expected_default_directory = path.dirname(path.realpath(__file__))
+        cls.non_default_input_directory = "/parse_test/"
+        cls.non_default_full_input_directory = "\parse_test"
+        cls.expected_default_directory = "\\"
         cls.directory_parameter_key = DIRECTORY_PARAMETER_VALID_KEYS[0]
         cls.prefix_parameter_key = PREFIX_PARAMETER_VALID_KEYS[0]
         cls.dry_run_parameter_key = DRY_RUN_PARAMETER_VALID_KEYS[0]
+
+    def setUp(self):
+        self.setUpPyfakefs()
+        self.fs.add_real_directory(path.dirname(path.realpath(__file__)))
 
     def validate_parsed_input_content(self, user_input, expected_directory, expected_prefix, is_dry_run):
         self.assertEqual(user_input.directory, expected_directory)
@@ -33,13 +36,13 @@ class TestUtils(unittest.TestCase):
             user_input, self.expected_default_directory, self.prefix_name, True)
 
     def test_parse_args_with_default_dry_run(self):
-        user_input = parse_args([self.directory_parameter_key, self.non_default_input_directory_suffix,
+        user_input = parse_args([self.directory_parameter_key, self.non_default_input_directory,
                                  self.prefix_parameter_key, self.prefix_name])
         self.validate_parsed_input_content(
             user_input, self.non_default_full_input_directory, self.prefix_name, False)
 
     def test_parse_args_with_non_default_parameters(self):
-        user_input = parse_args([self.directory_parameter_key, self.non_default_input_directory_suffix,
+        user_input = parse_args([self.directory_parameter_key, self.non_default_input_directory,
                                  self.prefix_parameter_key, self.prefix_name, self.dry_run_parameter_key])
         self.validate_parsed_input_content(
             user_input, self.non_default_full_input_directory, self.prefix_name, True)
